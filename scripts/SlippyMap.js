dd4c8bcb158cf036b32e9d4ea0e41472dce66aa8
Louis Parent <contact@louis-parent.fr>
2022-09-13 23:51:14 +0200

    Adding overlay groups

--- scripts/SlippyMap.js
+++ scripts/SlippyMap.js
@@ -5,40 +5,63 @@ class SlippyMap
 		attribution: "© OpenStreetMap"
 	};
 	
-	constructor(centerLatitude, centerLongitude, defaultZoom, maxZoom, id)
+	constructor(center, zoom, id)
 	{
 		this.center = {
-			latitude: centerLatitude,
-			longitude: centerLongitude
+			latitude: center.latitude,
+			longitude: center.longitude
 		};
 		
-		this.maxZoom = maxZoom;
-		
-		this.leaflet = L.map(id || "map").setView([centerLatitude, centerLongitude], defaultZoom);
+		this.maxZoom = zoom.max;		
 		
 		this.displayedRouting = null;
+		
+		this.setupTile();
+		this.setupLayerGroups();
+		this.setupLeaflet(zoom.default, id);
 	}
 	
-	setTile(tileConfig)
+	setupTile()
 	{
-		let config = tileConfig || SlippyMap.openStreetMapConfig;
-		
-		L.tileLayer(config.url, {
+		this.tile = L.tileLayer(SlippyMap.openStreetMapConfig.url, {
 			maxZoom: this.maxZoom,
-			attribution: config.attribution
-		}).addTo(this.leaflet);
+			attribution: SlippyMap.openStreetMapConfig.attribution
+		});
+	}
+	
+	setupLayerGroups()
+	{
+		this.coffeeGroup = L.layerGroup([]);
+		this.snackGroup = L.layerGroup([]);
+		
+		this.layerControl = L.control.layers({}, {
+			"Café": this.coffeeGroup,
+			"Snack": this.snackGroup
+		});
+	}
+	
+	setupLeaflet(defaultZoom, id)
+	{
+		this.leaflet = L.map(id || "map").setView([this.center.latitude, this.center.longitude], defaultZoom);
+		this.leaflet.addLayer(this.coffeeGroup);
+		this.leaflet.addLayer(this.snackGroup);
+		
+		this.tile.addTo(this.leaflet);
+		this.layerControl.addTo(this.leaflet);
 	}
 	
 	addCoffeeMarker(data)
 	{
-		const marker = L.marker([data.latitude, data.longitude], { icon: coffeeIcon }).addTo(this.leaflet);
+		const marker = L.marker([data.latitude, data.longitude], { icon: coffeeIcon });
 		marker.bindPopup("Disponibilité : <i>" + data.amount + " machines</i>");
+		this.coffeeGroup.addLayer(marker);
 	}
 	
 	addSnackMarker(data)
 	{
 		const marker = L.marker([data.latitude, data.longitude], { icon: snackIcon }).addTo(this.leaflet);
 		marker.bindPopup("Disponibilité : <i>" + data.amount + " machines</i>");
+		this.snackGroup.addLayer(marker);
 	}
 	
 	createPedestrianRouting(start, end)
